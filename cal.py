fb2bdc8df7a2003f4242a2df9f6a64749fe027c1
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-07 17:10:16 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,14 @@
+class cal():
+    
+    a = int(input("Enter the value of a:-"))
+    b = int(input("Enter the value of b:-"))
+    
+    def sum(a,b):
+        print("Sum:-",a + b) 
+    def mul(a,b):
+        print("Mul-:",a * b)
+        
+    sum(a,b)
+    mul(a,b)
+
+h = cal()
